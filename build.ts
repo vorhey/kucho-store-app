ce8a3d524fd74e3837b0977bf42fa3a08cf707e7
vorhey <jorgeherreraulloa@gmail.com>
2025-03-14 20:33:38 -0300

    remove github pages path

--- build.ts
+++ build.ts
@@ -153,7 +153,6 @@ const result = await build({
   minify: true,
   target: "browser",
   sourcemap: "linked",
-  publicPath: "https://vorhey.github.io/kucho-store-app/",
   define: {
     "process.env.NODE_ENV": JSON.stringify("production"),
   },
